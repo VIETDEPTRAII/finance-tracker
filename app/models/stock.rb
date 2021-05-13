3ae16b13868a5e6427ad6b5158b211ab5ac329ae
NGUYEN THANH VIET <nguyenthanhviet31031998@gmail.com>
2021-05-13 21:32:22 +0700

    Add annotate to models

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: stocks
+#
+#  id         :bigint           not null, primary key
+#  ticker     :string(255)
+#  name       :string(255)
+#  last_price :decimal(10, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Stock < ApplicationRecord
 end
