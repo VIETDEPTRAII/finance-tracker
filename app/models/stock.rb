fa5ee9880bbaaf68a33d3b7a2afb1c7bb0cd8b32
NGUYEN THANH VIET <nguyenthanhviet31031998@gmail.com>
2021-05-18 21:42:40 +0700

    Add env template file and new_lookup method to stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -10,4 +10,12 @@
 #  updated_at :datetime         not null
 #
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: ENV['IEX_PUBLISHABLE'],
+      secret_token: ENV['IEX_SECRET_TOKEN'],
+      endpoint: ENV['IEX_ENDPOINT']
+    )
+    client.price(ticker_symbol)
+  end
 end
