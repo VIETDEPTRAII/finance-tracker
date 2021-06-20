288f11af567ea680384498aa2b0d24af3627d669
NGUYEN THANH VIET <nguyenthanhviet31031998@gmail.com>
2021-06-20 21:22:58 +0700

    Add tracking stock feature

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,11 @@
 class UserStocksController < ApplicationController
+  def create
+    stock = Stock.find_by(ticker: params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      stock.save
+    end
+    UserStock.create(user: current_user, stock: stock)
+    redirect_to root_path, notice: "You have add #{stock.ticker} stock to your portfolio"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'users#my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-  resources :user_stocks
+  resources :user_stocks, only: %i[create]
 end
